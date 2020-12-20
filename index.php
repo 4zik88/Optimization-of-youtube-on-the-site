69dda8942786589876ebb3eeee00416625b44526
Vyacheslav <59470677+4zik88@users.noreply.github.com>
2020-12-20 12:43:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="video">
+                                  <!--Ваш ID YouTube-->
+        <a href="https://youtu.be/agck7F_yKX8" class="video__link" target="_blank">
+            <picture>
+                                                            <!--Ваш ID YouTube-->
+                <source srcset="https://i.ytimg.com/vi_webp/agck7F_yKX8/maxresdefault.webp" type="image/webp">
+                                                                                  <!--Ваш ID YouTube-->
+                <img class="video__media" src="https://i.ytimg.com/vi/agck7F_yKX8/maxresdefault.jpg" alt="Видео">
+            </picture>
+        </a>
+        <button class="video__button" aria-label="Запустить видео">
+            <svg width="68" height="48" viewBox="0 0 68 48"><path class="video__button-shape" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"></path><path class="video__button-icon" d="M 45,24 27,14 27,34"></path></svg>
+        </button>
+    </div>
+
+</body>
+</html>
